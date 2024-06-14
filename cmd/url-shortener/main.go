225188b030721315a00369c023dacda90b21e35b
Tarlan Huseynov <tarlan.huseynov@azintelecom.az>
2024-06-14 12:28:48 +0400

    storage added

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,15 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener-go/internal/config"
+	"url-shortener-go/internal/lib/logger/sl"
+	"url-shortener-go/internal/storage/sqlite"
 )
+
+const (
+	envLocal = "local"
+	envDev   = "development"
+	envProd  = "production"
+)
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg) // TODO: remove debug print
-	// TODO: init logger: slog
-	// TODO: init db: sqlite
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("Failed to initialize storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 	// TODO: init router: chi, chi render
 	// TODO: run server
 }
