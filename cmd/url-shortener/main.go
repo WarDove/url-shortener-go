081719530fabdd3fa85c99991bf21a892e9af574
Tarlan Huseynov <tarlan.huseynov@camlingroup.com>
2024-06-26 14:38:20 +0400

    init

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
 	"os"
 	"url-shortener-go/internal/config"
+	mvlogger "url-shortener-go/internal/http-server/middleware/logger"
+	"url-shortener-go/internal/lib/logger/handlers/slogpretty"
 	"url-shortener-go/internal/lib/logger/sl"
 	"url-shortener-go/internal/storage/sqlite"
 )
@@ -20,9 +24,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -34,6 +36,18 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
 
 func main() {
@@ -51,6 +65,18 @@ func main() {
 	}
 
 	_ = storage
+
+	router := chi.NewRouter()
+
+	// middleware
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(mvlogger.New(log)) // router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	// TODO: init router: chi, chi render
+	// TODO: run server
 
 	//id, err := storage.SaveURL("https://google.com", "google")
 	//if err != nil {
@@ -79,7 +105,4 @@ func main() {
 	//}
 	//
 	//log.Info("saved url", slog.Int64("id", id))
-
-	// TODO: init router: chi, chi render
-	// TODO: run server
 }
